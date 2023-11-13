34d54566b275a2c388555321c9797fae6facd2c5
Gian Franco <gianfraback@gmail.com>
2023-11-13 02:25:45 -0500

    feat: add routes to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
-import AppRouter from "./routes";
+import { UserRoute } from "./routes/users";
+import { UserModelMysql } from "./models/user/user.model.mysql";
+import { AppointmentRoute } from "./routes/appointments";
+import { AppointmentModelMysql } from "./models/appointment/appointment.model.mysql";
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/v1/", AppRouter);
+app.use("/api/v1/", UserRoute(UserModelMysql));
+app.use("/api/v1/", AppointmentRoute(AppointmentModelMysql));
 
 app.listen(3000, () => {
   console.log("listening on port 3000");
