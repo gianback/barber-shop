17a619eb68026f9cc5e716dbefa2d95af6fdc991
Gian Franco <gianfraback@gmail.com>
2023-11-13 02:26:32 -0500

    feat: add appointment route

---
+++ src/routes/appointments/index.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { AppointmentRepository } from "../../interfaces/appointment";
+import { AppointmentController } from "../../controllers/appointment.controller";
+
+export const AppointmentRoute = (appointmentModel: AppointmentRepository) => {
+  const appoinmentRoute = Router();
+
+  const appointmentController = new AppointmentController(appointmentModel);
+
+  appoinmentRoute.post("/", appointmentController.createAppointment);
+  appoinmentRoute.patch("/", appointmentController.editAppointment);
+  appoinmentRoute.get("/", appointmentController.getAppointmentsByUserid);
+
+  return appoinmentRoute;
+};
