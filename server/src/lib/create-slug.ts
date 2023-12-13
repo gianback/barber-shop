33a8d9742c251f0f3a196a46eaad7b34bbfd0101
Gian Franco <gianfraback@gmail.com>
2023-12-13 00:31:19 -0500

    refactor(server): refactor create slug function

--- server/src/lib/create-slug.ts
+++ server/src/lib/create-slug.ts
@@ -1,9 +1,17 @@
 export function createSlug(input: string): string {
   const slug = input.trim().replace(/\s+/g, "-");
 
-  const cleanSlug = slug.replace(/[^\w-]/g, "");
+  const cleanString = slug.replace(/[^\w\sáéíóúü]/g, "-");
+  const normalizedSlug = cleanString
+    .replace(/[áàãâä]/g, "a")
+    .replace(/[éèêë]/g, "e")
+    .replace(/[íìîï]/g, "i")
+    .replace(/[óòõôö]/g, "o")
+    .replace(/[úùûü]/g, "u")
+    .replace(/[ñ]/g, "n")
+    .replace(/[ç]/g, "c");
 
-  const lowercaseSlug = cleanSlug.toLowerCase();
+  const lowercaseSlug = normalizedSlug.toLowerCase();
 
   return lowercaseSlug;
 }
