6ee853ae8fe768f5161ade9da6619fe97ef398a0
Gian Franco <gianfraback@gmail.com>
2024-02-24 11:50:04 -0500

    feat(client): add footer

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,9 @@
+import { Container } from "./Container";
+
 export function Footer() {
-  return <footer>footer</footer>;
+  return (
+    <footer className="text-white py-4 md:py-12 lg:py-16 text-center bg-secondary border-t-primary border-t-[5px]  uppercase text-xl md:text-3xl font-medium">
+      <Container>TODOS LOS DERECHOS RESERVADOS @BARBER SHOP</Container>
+    </footer>
+  );
 }
